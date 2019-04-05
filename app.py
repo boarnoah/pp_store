3451a33082d90350cbdd2855925180e1deffdfcf
boarnoah <adipamihi@gmail.com>
2019-04-05 01:23:28 -0400

    Added sqlalchemy and pg db. Added wtf forms for the forms

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:swordfish@localhost:5432/postgres'
+db = SQLAlchemy(app)
 
+
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'Pizza, id: {self.id}, name: {self.name}'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    print("wololo")
+    app.run(port=8080, debug=True)
