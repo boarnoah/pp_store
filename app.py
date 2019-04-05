77689bcb06fc9957e80fa66bb23375f462c192e6
boarnoah <adipamihi@gmail.com>
2019-04-05 01:50:10 -0400

    Add simple pizza order for demonstration purposes.

--- app.py
+++ app.py
@@ -13,8 +13,21 @@ class Pizza(db.Model):
     def __repr__(self):
         return f'Pizza, id: {self.id}, name: {self.name}'
 
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    address = db.Column(db.String(80), unique=False, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey(Pizza.id), nullable=False)
+
+
 @app.route('/')
 def index():
+    # pizzas = Pizza.query.all()
+    # foo = Order(name='I C Wiener', address="1 Yonge St", pizza_id=pizzas[0].id)
+    #
+    # db.session.add(foo)
+    # db.session.commit()
+
     return render_template('index.html', pizzas=Pizza.query.all())
 
 if __name__ == '__main__':
