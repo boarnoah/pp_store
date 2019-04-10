7059adacd3541bf1e73dfacab1a8424ba4faaad9
Adipa Wijayathilaka <adipamihi@gmail.com>
2019-04-10 00:45:14 -0400

    Added page template, added pizza page + index store page. Improved models. Added catch for db timing out.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
+from sqlalchemy import exc as SQLAlchemyException
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ppdb:swordfish@ppdbmysql.ct4sai8mxgm9.us-east-2.rds.amazonaws.com:3306/ppdb'
@@ -9,15 +13,25 @@ db = SQLAlchemy(app)
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    short_desc = db.Column(db.String(80), unique=False, nullable=False)
+    long_desc = db.Column(db.String(280), unique=False, nullable=False)
+    img = db.Column(db.String(280), unique=False, nullable=False)
 
     def __repr__(self):
-        return 'Pizza, id: {}, name: {}'.format(self.id, self.name)
+        return 'Pizza, id: {}, name: {}, price: {}'.format(self.id, self.name, self.price)
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
     address = db.Column(db.String(80), unique=False, nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey(Pizza.id), nullable=False)
+    pizza_count = db.Column(db.Integer, unique=False, nullable=False)
+
+
+class OrderForm (FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    address = StringField('name', validators=[DataRequired()])
 
 # ex: how to add entries to db
 # foo = Order(name='I C Wiener', address="1 Yonge St", pizza_id=pizzas[0].id)
@@ -27,8 +41,16 @@ class Order(db.Model):
     
 @app.route('/')
 def index():
-    pizzas = Pizza.query.all()
+    try:
+        pizzas = Pizza.query.all()
+    except SQLAlchemyException.OperationalError:
+        pizzas = []
+
     return render_template('index.html', pizzas=pizzas) #Renders template, with list queried from db
+
+@app.route('/pizza')
+def pizza():
+    return render_template('pizza.html')
 
 if __name__ == '__main__':
     print("wololo")
