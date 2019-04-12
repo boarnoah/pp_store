33e96c484ac0d870a06d887573298dbe6ccd770b
boarnoah <adipamihi@gmail.com>
2019-04-12 00:18:32 -0400

    Added order pizza form

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, NumberRange
 from sqlalchemy import exc as SQLAlchemyException
+
+import secrets
 
 app = Flask(__name__)
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'mysql://ppdb:swordfish@ppdbmysql.ct4sai8mxgm9.us-east-2.rds.amazonaws.com:3306/ppdb'
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = secrets.token_bytes(32)
+app.config['WTF_CSRF_SECRET_KEY'] = secrets.token_bytes(32)
 
+csrf = CSRFProtect(app)
 
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -36,8 +41,9 @@ class Order(db.Model):
 
 
 class OrderForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    address = StringField('name', validators=[DataRequired()])
+    user_name = StringField('user_name', validators=[DataRequired()])
+    address = StringField('address', validators=[DataRequired()])
+    pizza_count = IntegerField('pizza_count', validators=[DataRequired(), NumberRange(min=1, max=20)])
 
 
 # ex: how to add entries to db
@@ -56,23 +62,41 @@ def index():
     return render_template('index.html', pizzas=pizzas)  # Renders template, with list queried from db
 
 
-@app.route('/pizza/<pizza_id>')
+@app.route('/pizza/<pizza_id>', methods=['GET', 'POST'])
 def pizza(pizza_id):
     pizza = Pizza.query.filter_by(id=pizza_id).first()
+    form = OrderForm(request.form)
+
+    if form.validate_on_submit():
+        new_order = Order(name=form.user_name.data, address=form.address.data, pizza_id=pizza_id,
+                          pizza_count=form.pizza_count.data)
+
+        if new_order:
+            db.session.add(new_order)
+            db.session.commit()
+
+        return redirect('/success')
 
     if pizza:
-        return render_template('pizza.html', pizza=pizza)
+        return render_template('pizza.html', pizza=pizza, form=form)
     else:
         return render_template("404.html")
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
 
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+
 @app.route("/ssh")
 def inside_joke():
     return render_template('inside_joke.html')
+
 
 if __name__ == '__main__':
     print("wololo")
